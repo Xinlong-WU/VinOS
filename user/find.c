b1f8084525afd211be538cdee4e8fb1e24e7d230
XinlongWu <821408745@qq.com>
2021-12-29 18:50:33 +0800

    lab 1.5 find (moderate) [2/2]

--- user/find.c
+++ user/find.c
@@ -13,22 +13,29 @@ char * fmtpath(char * path){
     return path;
 }
 
-void removeFileName(char * path){
-    char *p;
+char const * getFileName(char const * path){
+    static char buf[DIRSIZ+1];
+    char const *p;
 
     // Find first character after last slash.
     for(p=path+strlen(path); p >= path && *p != '/'; p--)
         ;
     p++;
-    *(p) = '\0';
-    return;
+
+    // Return blank-padded name.
+    if(strlen(p) >= DIRSIZ)
+        return p;
+    memmove(buf, p, strlen(p));
+    memset(buf+strlen(p), ' ', DIRSIZ-strlen(p));
+    return buf;
 }
 
-void find(int fd,char const * path){
+void find(int fd,char const * path, char const * patten){
     char * buf = malloc(sizeof(char)*100);
     struct dirent de;
     struct stat st;
     int subfd;
+    int enableFilter = (strcmp(patten, "*") != 0);
 
     memmove(buf, path, strlen(path));
 
@@ -47,7 +54,8 @@ void find(int fd,char const * path){
         if(de.inum == 0)
             continue;
         if(strcmp(de.name,".") == 0){
-            printf("%s \n",path);
+            if(!enableFilter)
+                printf("%s \n",path);
             continue;
         }
         if(strcmp(de.name,"..") == 0)
@@ -61,12 +69,19 @@ void find(int fd,char const * path){
             continue;
         }
         if (st.type == T_DIR){
-            find(subfd,fmtpath(buf));
+            find(subfd,fmtpath(buf), patten);
             close(subfd);
             continue;
         }
         close(subfd);
 
+        if (enableFilter)
+        {
+            if (strcmp(patten,de.name) == 0){
+                printf("%s \n",buf);
+            }
+            continue;
+        }
         printf("%s \n",buf);
     }
     free(buf);
@@ -76,17 +91,29 @@ int main(int argc, char const *argv[])
 {
     int fd;
     char * path = malloc(sizeof(char)*100);
+    char * patten = malloc(sizeof(char)*100);
     if (argc < 2)
     {
-        path = memmove(path,"./",strlen("./"));
+        path = memmove(path,"./\0",strlen("./\0"));
+        patten = memmove(patten,"*\0",strlen("*\0"));
+    }
+    else if (argc > 2)
+    {
+        printf("Error: Too many Arguments.\n");
+        printf("find <directory> [filename]\n");
+        free(path);
+        free(patten);
+        exit(0);
     }
     else
     {
         path = memmove(path,argv[1],strlen(argv[1]));
+        patten = memmove(patten,argv[2],strlen(argv[2]));
     }
     fd = open(path,O_RDONLY);
-    find(fd,fmtpath(path));
+    find(fd,fmtpath(path),patten);
     free(path);
+    free(patten);
     close(fd);
     exit(0);
 }
